f08713948cd994a7dea1c66255e9ac8f67c0e3d6
david <dlonicholas@gmail.com>
2021-10-10 17:09:59 -0600

    Moved date logic to app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import {differenceInCalendarDays} from "date-fns";
+
+import "./normalize.css";
+import "./index.css";
 import "./App.css";
 import Card from "./UI/Card";
 import AppHeader from "./components/AppHeader";
 import DaysTo from "./components/DaysTo";
 
 function App() {
+	const curDate = new Date();
+	const thisYear = curDate.getFullYear();
+	let halloween = new Date(thisYear, 9, 31);
+	let daysDiff = differenceInCalendarDays(halloween, curDate);
+
+	if (daysDiff < 0) {
+		halloween = halloween.setFullYear(thisYear + 1);
+		daysDiff = differenceInCalendarDays(halloween, curDate);
+	}
 	return (
-		<div className="App">
+		<div className={`App ${daysDiff ? `App__not` : "App_halloween"}`}>
 			<AppHeader />
 			<Card>
-				<DaysTo />
+				<DaysTo daysDiff={daysDiff} />
 			</Card>
 		</div>
 	);
--- src/components/DaysTo.js
+++ src/components/DaysTo.js
@@ -1,18 +1,7 @@
-import {differenceInCalendarDays} from "date-fns";
 import NotHalloween from "./NotHalloween";
 import Halloween from "./Halloween";
 
-const DaysTo = () => {
-	const curDate = new Date();
-	const thisYear = curDate.getFullYear();
-	let halloween = new Date(thisYear, 9, 31);
-	let daysDiff = differenceInCalendarDays(halloween, curDate);
-
-	if (daysDiff < 0) {
-		halloween = halloween.setFullYear(thisYear + 1);
-		daysDiff = differenceInCalendarDays(halloween, curDate);
-	}
-
+const DaysTo = ({daysDiff}) => {
 	if (daysDiff === 0) {
 		return <Halloween />;
 	} else {
