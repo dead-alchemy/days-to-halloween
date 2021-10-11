b5dc219b5660ccfb349492e51310378e84cb35af
david <dlonicholas@gmail.com>
2021-10-10 18:03:53 -0600

    Card component now support custom classes

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,5 +1,9 @@
 const Card = (props) => {
-	return <section className="Card">{props.children}</section>;
+	return (
+		<section className={`Card ${props.className}`}>
+			{props.children}
+		</section>
+	);
 };
 
 export default Card;
